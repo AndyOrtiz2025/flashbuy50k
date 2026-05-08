cfb9cbc3853f3c790263f99b48608fdfa6bf3693
DESKTOP-0K9V1P1\HP <nafer627558@gmail.com>
2026-05-07 21:45:24 -0600

    Agregar gateway y configuraciones

---
+++ gateway/src/main/java/com/flashbuy/gateway/RateLimitConfig.java
@@ -0,0 +1,19 @@
+package com.flashbuy.gateway;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        // Rate limiting por IP
+        return exchange -> Mono.just(
+            exchange.getRequest().getRemoteAddress().getAddress().getHostAddress()
+        );
+    }
+}
